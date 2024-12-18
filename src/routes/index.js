4fe7558b4c24d158526056ca5399107041a77444
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2024-12-18 14:11:22 -0500

    [FIX] remove test routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 import employeeRoutes from './employeeRoutes.js'
 import paymentRoutes from './paymentRoutes.js'
-import testRoutes from './testRoutes.js'
 import weatherRoutes from './weatherRoutes.js'
 
 export {
   employeeRoutes,
   paymentRoutes,
-  testRoutes,
   weatherRoutes
 }
